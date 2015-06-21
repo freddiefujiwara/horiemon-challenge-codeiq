0febd39b688c3c42e7cde1ff88bed770339613e8
Fumikazu Fujiwara <github@ze.gs>
2015-06-21 12:31:02 +0900

    Caluclate is finished

--- horiemon.go
+++ horiemon.go
@@ -5,7 +5,14 @@ func Target(input []int) int {
 }
 func Except(pair []int, array []int) []int {
 	var except []int
-	for _, v := range array {
+	var start int
+	for i, v := range array {
+		if v == pair[1] {
+			start = i + 1
+		}
+
+	}
+	for _, v := range array[start:] {
 		if v != pair[0] && v != pair[1] {
 			except = append(except, v)
 		}
@@ -21,4 +28,15 @@ func ListUp(array []int) [][]int {
 		}
 	}
 	return list
+}
+func Calculate(target int, array []int) [][]int {
+	var answeres [][]int
+	for _, pair := range ListUp(array) {
+		for _, v := range Except(pair, array) {
+			if target == pair[0]+pair[1]+v {
+				answeres = append(answeres, []int{pair[0], pair[1], v})
+			}
+		}
+	}
+	return answeres
 }
--- horiemon_test.go
+++ horiemon_test.go
@@ -13,8 +13,8 @@ func TestTarget(t *testing.T) {
 	}
 }
 func TestListUp(t *testing.T) {
-	actual := ListUp([]int{5, 8, 4, 10, 3, 2})
-	expected := [][]int{{5, 8}, {5, 4}, {5, 10}, {5, 3}, {5, 2},
+	actual := ListUp([]int{8, 4, 10, 3, 2})
+	expected := [][]int{
 		{8, 4}, {8, 10}, {8, 3}, {8, 2},
 		{4, 10}, {4, 3}, {4, 2},
 		{10, 3}, {10, 2},
@@ -26,6 +26,23 @@ func TestListUp(t *testing.T) {
 func TestExcept(t *testing.T) {
 	actual := Except([]int{5, 8}, []int{5, 8, 4, 10, 3, 2})
 	expected := []int{4, 10, 3, 2}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+	actual = Except([]int{8, 4}, []int{5, 8, 4, 10, 3, 2})
+	expected = []int{10, 3, 2}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+}
+func TestCalculate(t *testing.T) {
+	actual := Calculate(15, []int{8, 4, 10, 3, 2})
+	expected := [][]int{{8, 4, 3}, {10, 3, 2}}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+	actual = Calculate(35, []int{13, 12, 17, 10, 4, 18, 3, 11, 5, 7})
+	expected = [][]int{{13, 12, 10}, {13, 17, 5}, {13, 4, 18}, {12, 18, 5}, {17, 11, 7}, {10, 18, 7}}
 	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("got %v\nwant %v", actual, expected)
 	}
