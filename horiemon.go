74929ad577758c350882fa4fa5827bbc36302172
Fumikazu Fujiwara <github@ze.gs>
2015-06-21 11:47:36 +0900

    use reflect.DeepEqual

--- horiemon.go
+++ horiemon.go
@@ -3,3 +3,11 @@ package Horiemon
 func Target(input []int) int {
 	return input[0]
 }
+
+func ListUp(array []int) [][]int {
+	return [][]int{{5, 8}, {5, 4}, {5, 10}, {5, 3}, {5, 2},
+		{8, 4}, {8, 10}, {8, 3}, {8, 2},
+		{4, 10}, {4, 3}, {4, 2},
+		{10, 3}, {10, 2},
+		{3, 2}}
+}
--- horiemon_test.go
+++ horiemon_test.go
@@ -1,13 +1,25 @@
 package Horiemon
 
 import (
+	"reflect"
 	"testing"
 )
 
-func TestSum(t *testing.T) {
-	actual := Target([]int{1, 2, 3, 4, 5})
-	expected := 1
-	if actual != expected {
+func TestTarget(t *testing.T) {
+	actual := Target([]int{15, 5, 8, 4, 10, 3, 2})
+	expected := 15
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+}
+func TestListUp(t *testing.T) {
+	actual := ListUp([]int{5, 8, 4, 10, 3, 2})
+	expected := [][]int{{5, 8}, {5, 4}, {5, 10}, {5, 3}, {5, 2},
+		{8, 4}, {8, 10}, {8, 3}, {8, 2},
+		{4, 10}, {4, 3}, {4, 2},
+		{10, 3}, {10, 2},
+		{3, 2}}
+	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("got %v\nwant %v", actual, expected)
 	}
 }
